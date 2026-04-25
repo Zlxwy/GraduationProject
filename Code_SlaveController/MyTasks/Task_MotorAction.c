bd85a4314afb578d39b414b51b5ac5a2adf13e98
Zlxwy <y3021330020@163.com>
2026-04-26 02:14:09 +0800

    允许平面的两个步进电机同时运动

--- Code_SlaveController/MyTasks/Task_MotorAction.c
+++ Code_SlaveController/MyTasks/Task_MotorAction.c
@@ -30,27 +30,62 @@ void MotorActionTaskFunc(void *argument) {
     Msg_MotorAction_t msg;
     if ( osMessageQueueGet(gMsgQueueMotorAction, &msg, NULL, 500) == osOK ) {
       if (msg.RequestType == MotorActionRequestType_BasicMove) { // 基础行走
-        // StepperMotor_MoveSteps( // 步进电机移动指定步数函数
-        //   &gStepperMotorShoulder, // 肩关节步进电机
-        //   msg.Steps, // 行走步数
-        //   msg.Speed // 行走速度
-        // ); // Object, steps, speed
-        StepperMotor_t *pStepperMotor = NULL;
-        switch (msg.MotorID) { // 根据电机ID选择步进电机对象，进行操作
-          case 0x00: pStepperMotor = &gStepperMotorShoulder; break; // 肩关节步进电机
-          case 0x01: pStepperMotor = &gStepperMotorElbow; break; // 肘关节步进电机
-          case 0x02: pStepperMotor = &gStepperMotorLift; break; // 竖轴步进电机
-          default: break;
-        }
+
         if (msg.ActionType == 0x00) { // 动作类型：0x00停止
-          StepperMotor_Stop(pStepperMotor); // 步进电机停止
+          StepperMotor_t *pStepperMotor = NULL;
+          switch (msg.MotorID) { // 根据电机ID选择步进电机对象，进行操作
+            case 0x00: pStepperMotor = &gStepperMotorShoulder; break; // 肩关节步进电机
+            case 0x01: pStepperMotor = &gStepperMotorElbow; break; // 肘关节步进电机
+            case 0x02: pStepperMotor = &gStepperMotorLift; break; // 竖轴步进电机
+            default: break;
+          }
+          if (pStepperMotor != NULL) StepperMotor_Stop(pStepperMotor); // 步进电机停止
         }
+        
         else if (msg.ActionType == 0x01) { // 动作类型：0x01旋转指定步数
-          StepperMotor_MoveSteps(pStepperMotor, msg.Steps, msg.Speed); // 步进电机移动指定步数函数
-          while (StepperMotor_GetRunState(pStepperMotor) == StepperMotor_RunState_MoveSteps); // 等待步进电机移动完成
+          StepperMotor_t *pStepperMotor = NULL;
+          switch (msg.MotorID) { // 根据电机ID选择步进电机对象，进行操作
+            case 0x00: // 肩关节步进电机
+              while ( // 移动肩关节要等待肩关节和竖轴移动完成
+                StepperMotor_GetRunState(&gStepperMotorShoulder) == StepperMotor_RunState_MoveSteps
+                || StepperMotor_GetRunState(&gStepperMotorShoulder) == StepperMotor_RunState_RunContinuous
+                || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_MoveSteps
+                || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_RunContinuous
+              ); pStepperMotor = &gStepperMotorShoulder;
+              break;
+            case 0x01: // 肘关节步进电机
+              while ( // 移动肘关节要等待肘关节和竖轴移动完成
+                StepperMotor_GetRunState(&gStepperMotorElbow) == StepperMotor_RunState_MoveSteps
+                || StepperMotor_GetRunState(&gStepperMotorElbow) == StepperMotor_RunState_RunContinuous
+                || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_MoveSteps
+                || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_RunContinuous
+              ); pStepperMotor = &gStepperMotorElbow;
+              break;
+            case 0x02: // 竖轴步进电机
+              while ( // 移动竖轴要等待所有电机都移动完成
+                StepperMotor_GetRunState(&gStepperMotorShoulder) == StepperMotor_RunState_MoveSteps
+                || StepperMotor_GetRunState(&gStepperMotorShoulder) == StepperMotor_RunState_RunContinuous
+                || StepperMotor_GetRunState(&gStepperMotorElbow) == StepperMotor_RunState_MoveSteps
+                || StepperMotor_GetRunState(&gStepperMotorElbow) == StepperMotor_RunState_RunContinuous
+                || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_MoveSteps
+                || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_RunContinuous
+              ); pStepperMotor = &gStepperMotorLift;
+              break;
+            default: break;
+          }
+          if (pStepperMotor != NULL) StepperMotor_MoveSteps(pStepperMotor, msg.Steps, msg.Speed); // 步进电机移动指定步数函数
+          // while (StepperMotor_GetRunState(pStepperMotor) == StepperMotor_RunState_MoveSteps); // 等待步进电机移动完成
         } 
+        
         else if (msg.ActionType == 0x02) { // 动作类型：0x02持续旋转（Steps为-1则反转，Steps为1则正转）
-          StepperMotor_RunContinuous( // 步进电机持续旋转函数
+          StepperMotor_t *pStepperMotor = NULL;
+          switch (msg.MotorID) { // 根据电机ID选择步进电机对象，进行操作
+            case 0x00: pStepperMotor = &gStepperMotorShoulder; break; // 肩关节步进电机
+            case 0x01: pStepperMotor = &gStepperMotorElbow; break; // 肘关节步进电机
+            case 0x02: pStepperMotor = &gStepperMotorLift; break; // 竖轴步进电机
+            default: break;
+          }
+          if (pStepperMotor != NULL) StepperMotor_RunContinuous( // 步进电机持续旋转函数
             pStepperMotor, // 步进电机对象
             msg.Steps > 0 ? // 步进电机步数是否为1
             StepperMotor_Direction_Forward : // 步数为1则方向正转
@@ -58,9 +93,7 @@ void MotorActionTaskFunc(void *argument) {
             msg.Speed // 步进电机速度
           );
         }
-        else if (pStepperMotor == NULL) { // 未知步进电机
-          UartDmaStream_DebugPrintf(&gMainStream, "MotorActionTaskFunc: 传入未知的电机[%d]\n", msg.MotorID);
-        }
+        
         else { // 未知动作类型
           UartDmaStream_DebugPrintf(&gMainStream, "MotorActionTaskFunc: 传入未知的动作类型[%d]\n", msg.ActionType);
         }
@@ -69,18 +102,33 @@ void MotorActionTaskFunc(void *argument) {
 
 
       else if (msg.RequestType == MotorActionRequestType_MoveOnPlane) { // 平面行走
-        ;
+        StepperMotor_t *pStepperMotor = NULL;
+        switch (msg.MotorID) { // 根据电机ID选择步进电机对象，进行操作
+          case 0x00: pStepperMotor = &gStepperMotorShoulder; break; // 肩关节步进电机
+          case 0x01: pStepperMotor = &gStepperMotorElbow; break; // 肘关节步进电机
+          case 0x02: pStepperMotor = &gStepperMotorLift; break; // 竖轴步进电机
+          default: break;
+        }
+        (void)pStepperMotor;;;
       } // else if (msg.RequestType == MotorActionRequestType_MoveOnPlane)
 
 
 
       else if (msg.RequestType == MotorActionRequestType_VerticalAxisRst) { // 竖轴位置重置
+        while ( // 移动竖轴要等待所有电机都移动完成
+          StepperMotor_GetRunState(&gStepperMotorShoulder) == StepperMotor_RunState_MoveSteps
+          || StepperMotor_GetRunState(&gStepperMotorShoulder) == StepperMotor_RunState_RunContinuous
+          || StepperMotor_GetRunState(&gStepperMotorElbow) == StepperMotor_RunState_MoveSteps
+          || StepperMotor_GetRunState(&gStepperMotorElbow) == StepperMotor_RunState_RunContinuous
+          || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_MoveSteps
+          || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_RunContinuous
+        );
         if ( DigitalInput_IsTriggered(&gLimitSwitchVerticalAxis) ) { // 如果竖轴限位开关此时被触发着
-          StepperMotor_MoveSteps(&gStepperMotorLift, 200, 500); // 竖轴步进电机先往离开限位器方向转200步
+          StepperMotor_MoveSteps(&gStepperMotorLift, 200, 1000); // 竖轴步进电机先往离开限位器方向转200步
           while (StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_MoveSteps); // 等待步进电机移动指定步数完成
         }
         IsLimitSwitchVerticalAxisExtiEnabled = true; // 竖轴限位开关中断使能标志位
-        StepperMotor_RunContinuous(&gStepperMotorLift, StepperMotor_Direction_Backward, 500); // 竖轴步进电机持续往限位器方向转
+        StepperMotor_RunContinuous(&gStepperMotorLift, StepperMotor_Direction_Backward, 1000); // 竖轴步进电机持续往限位器方向转
         while (StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_RunContinuous); // 等待步进电机持续移动完成
 
       } // else if (msg.RequestType == MotorActionRequestType_VerticalAxisRst)
@@ -89,13 +137,21 @@ void MotorActionTaskFunc(void *argument) {
 
       else if (msg.RequestType == MotorActionRequestType_VerticalAxisMove) { // 竖轴位置
         int32_t steps = 1230.0f * msg.PositionZ / 100.0f; // 计算出要下降的步数
-        StepperMotor_MoveSteps(&gStepperMotorLift, steps, 500); // 竖轴步进电机移动指定步数函数
+        StepperMotor_MoveSteps(&gStepperMotorLift, steps, 1000); // 竖轴步进电机移动指定步数函数
         while (StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_MoveSteps); // 等待步进电机移动完成
       } // else if (msg.RequestType == MotorActionRequestType_VerticalAxisMove)
 
 
 
       else if (msg.RequestType == MotorActionRequestType_SetMagnetStatus) { // 设置磁铁状态
+        while ( // 设置磁铁要等待所有电机都移动完成
+          StepperMotor_GetRunState(&gStepperMotorShoulder) == StepperMotor_RunState_MoveSteps
+          || StepperMotor_GetRunState(&gStepperMotorShoulder) == StepperMotor_RunState_RunContinuous
+          || StepperMotor_GetRunState(&gStepperMotorElbow) == StepperMotor_RunState_MoveSteps
+          || StepperMotor_GetRunState(&gStepperMotorElbow) == StepperMotor_RunState_RunContinuous
+          || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_MoveSteps
+          || StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_RunContinuous
+        );
         ElectronMagnet_SetStatus(msg.MagnetStatus); // 根据传入的磁铁状态设置磁铁
       } // else if (msg.RequestType == MotorActionRequestType_SetMagnetStatus)
 
@@ -112,3 +168,124 @@ void MotorActionTaskFunc(void *argument) {
     // 读取队列就已经包含延时操作
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*这个线程用来执行所有电机的动作*/
+// void MotorActionTaskFunc(void *argument) {
+//   (void)argument;
+//   while (!IsAllInitOkay) { osDelay(100); }
+
+//   while (true) {
+//     Msg_MotorAction_t msg;
+//     if ( osMessageQueueGet(gMsgQueueMotorAction, &msg, NULL, 500) == osOK ) {
+//       if (msg.RequestType == MotorActionRequestType_BasicMove) { // 基础行走
+//         // StepperMotor_MoveSteps( // 步进电机移动指定步数函数
+//         //   &gStepperMotorShoulder, // 肩关节步进电机
+//         //   msg.Steps, // 行走步数
+//         //   msg.Speed // 行走速度
+//         // ); // Object, steps, speed
+//         StepperMotor_t *pStepperMotor = NULL;
+//         switch (msg.MotorID) { // 根据电机ID选择步进电机对象，进行操作
+//           case 0x00: pStepperMotor = &gStepperMotorShoulder; break; // 肩关节步进电机
+//           case 0x01: pStepperMotor = &gStepperMotorElbow; break; // 肘关节步进电机
+//           case 0x02: pStepperMotor = &gStepperMotorLift; break; // 竖轴步进电机
+//           default: break;
+//         }
+//         if (msg.ActionType == 0x00) { // 动作类型：0x00停止
+//           StepperMotor_Stop(pStepperMotor); // 步进电机停止
+//         }
+//         else if (msg.ActionType == 0x01) { // 动作类型：0x01旋转指定步数
+//           StepperMotor_MoveSteps(pStepperMotor, msg.Steps, msg.Speed); // 步进电机移动指定步数函数
+//           while (StepperMotor_GetRunState(pStepperMotor) == StepperMotor_RunState_MoveSteps); // 等待步进电机移动完成
+//         } 
+//         else if (msg.ActionType == 0x02) { // 动作类型：0x02持续旋转（Steps为-1则反转，Steps为1则正转）
+//           StepperMotor_RunContinuous( // 步进电机持续旋转函数
+//             pStepperMotor, // 步进电机对象
+//             msg.Steps > 0 ? // 步进电机步数是否为1
+//             StepperMotor_Direction_Forward : // 步数为1则方向正转
+//             StepperMotor_Direction_Backward, // 步数为-1则方向反转
+//             msg.Speed // 步进电机速度
+//           );
+//         }
+//         else if (pStepperMotor == NULL) { // 未知步进电机
+//           UartDmaStream_DebugPrintf(&gMainStream, "MotorActionTaskFunc: 传入未知的电机[%d]\n", msg.MotorID);
+//         }
+//         else { // 未知动作类型
+//           UartDmaStream_DebugPrintf(&gMainStream, "MotorActionTaskFunc: 传入未知的动作类型[%d]\n", msg.ActionType);
+//         }
+//       } // if (msg.RequestType == MotorActionRequestType_BasicMove)
+
+
+
+//       else if (msg.RequestType == MotorActionRequestType_MoveOnPlane) { // 平面行走
+//         ;
+//       } // else if (msg.RequestType == MotorActionRequestType_MoveOnPlane)
+
+
+
+//       else if (msg.RequestType == MotorActionRequestType_VerticalAxisRst) { // 竖轴位置重置
+//         if ( DigitalInput_IsTriggered(&gLimitSwitchVerticalAxis) ) { // 如果竖轴限位开关此时被触发着
+//           StepperMotor_MoveSteps(&gStepperMotorLift, 200, 500); // 竖轴步进电机先往离开限位器方向转200步
+//           while (StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_MoveSteps); // 等待步进电机移动指定步数完成
+//         }
+//         IsLimitSwitchVerticalAxisExtiEnabled = true; // 竖轴限位开关中断使能标志位
+//         StepperMotor_RunContinuous(&gStepperMotorLift, StepperMotor_Direction_Backward, 500); // 竖轴步进电机持续往限位器方向转
+//         while (StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_RunContinuous); // 等待步进电机持续移动完成
+
+//       } // else if (msg.RequestType == MotorActionRequestType_VerticalAxisRst)
+
+
+
+//       else if (msg.RequestType == MotorActionRequestType_VerticalAxisMove) { // 竖轴位置
+//         int32_t steps = 1230.0f * msg.PositionZ / 100.0f; // 计算出要下降的步数
+//         StepperMotor_MoveSteps(&gStepperMotorLift, steps, 500); // 竖轴步进电机移动指定步数函数
+//         while (StepperMotor_GetRunState(&gStepperMotorLift) == StepperMotor_RunState_MoveSteps); // 等待步进电机移动完成
+//       } // else if (msg.RequestType == MotorActionRequestType_VerticalAxisMove)
+
+
+
+//       else if (msg.RequestType == MotorActionRequestType_SetMagnetStatus) { // 设置磁铁状态
+//         ElectronMagnet_SetStatus(msg.MagnetStatus); // 根据传入的磁铁状态设置磁铁
+//       } // else if (msg.RequestType == MotorActionRequestType_SetMagnetStatus)
+
+
+
+//       else { // 未知请求
+//         ;
+//       } // else
+
+
+
+//     }
+
+//     // 读取队列就已经包含延时操作
+//   }
+// }
+
